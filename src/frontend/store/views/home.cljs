bebbbddc10808591a27ec0ce66fe8301f8c1d6ce
Mylab <labs@onmycrowd.com>
2019-04-28 09:31:14 -0700

    Registering signup to the navigation.

--- src/frontend/store/views/home.cljs
+++ src/frontend/store/views/home.cljs
@@ -1,13 +1,18 @@
 (ns store.views.home
   (:require [reagent.core  :as reagent]
             [re-frame.core :refer [subscribe dispatch]]
-            [store.views.landing :as lp]))
+            [store.views.landing :as lp]
+            [store.views.signup :as su]))
 
 (defmulti render-view (fn [v & args] v))
 
 (defmethod render-view :home
            [_ s]
            (lp/landing))
+
+(defmethod render-view :signup
+           [_ s]
+           (su/signup))
 
 (defn current
   []
