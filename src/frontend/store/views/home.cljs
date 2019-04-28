47a8abe8e24599688e203bd342c1e41e21494c6d
Mylab <labs@onmycrowd.com>
2019-04-27 21:35:49 -0700

    Segragating a bit more the UI in orther to code better.

--- src/frontend/store/views/home.cljs
+++ src/frontend/store/views/home.cljs
@@ -1,16 +1,15 @@
 (ns store.views.home
   (:require [reagent.core  :as reagent]
             [re-frame.core :refer [subscribe dispatch]]
-            [clojure.string :as str]))
+            [store.views.landing :as lp]))
 
-(defn home
-      []
-      [:div
-       [:main {:role "main"}
-        [:section.jumbotron.text-center
-         [:div.container
-          [:h1.jumbotron-heading "Album example"]
-          [:p.lead.text-muted "Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely."]
-          [:p
-           [:a {:href "#" :class "btn btn-primary my-2"} "Main call to action"]
-           [:a {:href "#" :class "btn btn-secondary my-2"} "Secondary action"]]]]]])
+(defmulti render-view (fn [v & args] v))
+
+(defmethod render-view :home
+           [_ s]
+           (lp/landing))
+
+(defn current
+  []
+  (fn []
+    (render-view @(subscribe [:showing]))))
